5ab50df942597c9ea5b5719c73a34f9b5c4bb02f
zc00l <andre.marques@esecurity.com.br>
2018-04-29 16:07:36 -0400

    Removing useless folders and adding updated .gitignore

--- build/lib.linux-x86_64-2.7/shellpop/__init__.py
+++
@@ -1,4 +0,0 @@
-from bind import *
-from reverse import *
-from encoders import *
-from classes import *
--- build/lib.linux-x86_64-2.7/shellpop/encoders.py
+++
@@ -1,9 +0,0 @@
-def powershell_base64(data):
-    """
-    Encode something compatible for Powershell base64-encoding
-    @zc00l
-    """
-    out = ""
-    for char in data:
-        out += char + "\x00"
-    return out.encode("base64").replace("\n", "")
