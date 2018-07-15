c6d7d0fe8136d4839931ad24d107bf44d48be531
Andrew LeFevre <jalefevre@liberty.edu>
2018-07-14 23:13:08 -0400

    IP obfuscation option now also obfuscates the port number

--- src/obfuscators.py
+++ src/obfuscators.py
@@ -28,7 +28,10 @@ def gen_random_var(smallVars):
         minVarLen = 6
         maxVarLen = 15
 
-    randVarLen = random.randint(minVarLen, maxVarLen)
+    randVarLen = 0
+    while randVarLen < minVarLen:
+        randVarLen = ord(os.urandom(1)) % (maxVarLen + 1)
+
     randomVar = "".join(string.ascii_letters[ord(os.urandom(1)) % 52] for x in range(randVarLen))
 
     return randomVar
@@ -98,9 +101,81 @@ def random_base_ip_gen(parts, smallIP):
                 else:
                     randBaseIP += octParts[i] + '.'
 
+        # Check to make sure all four parts of IP aren't decimal... in which case nothing would have changed
         if sum(baseChoices) > 0:
             ip_obfuscated = True
         else:
             baseChoices.clear()
 
     return randBaseIP[:-1]
+
+def obfuscate_port(port, smallPort):
+    """
+    Obfuscate a port number by replacing the single int
+    with an arithmetic expression. Returns a string that
+    when evaluated mathmatically, is equal to the port entered.
+    @capnspacehook 
+    """
+    modPieces = gen_mod_expr(port)
+
+    if smallPort:
+        return "(%s*%s+%s)" % (modPieces[0], modPieces[1], modPieces[2])
+    else:
+        portStr = ""
+        portStrPieces = []
+        for part in modPieces:
+            if part != 0:
+                choice = ord(os.urandom(1)) % 2
+                if choice:
+                    portStrPieces.append(gen_simple_expr(part))
+                else:
+                    part = gen_mod_expr(part)
+                    portStrPieces.append("(%s*%s+%s)" % (part[0], part[1], part[2]))
+            else:
+                portStrPieces.append("0")
+
+        return "(%s*%s+%s)" % (portStrPieces[0], portStrPieces[1], portStrPieces[2])
+                
+            
+def gen_mod_expr(n):
+    if n == 1:
+        right = n
+    else:
+        right = random.randint(1, int(n / 2))
+    left = int(n / right)
+    remainder = n % right    
+    
+    return (left, right, remainder)
+
+def gen_simple_expr(n):
+    choice = ord(os.urandom(1)) % 3
+    left = 0
+    if choice == 0:
+        left = random.randint(-n * 2, n - 1)
+        right = random.randint(-n + 1, n * 2)
+        if left + right < n:
+            offset = n - (left + right)
+            expr = "(%s+%s)--%s" % (left, right, offset)
+        else:
+            offset = (left + right) - n
+            expr = "((%s+%s)-%s)" % (left, right, offset)
+    elif choice == 1:
+        left = random.randint(-n + 1, n * 2)
+        right = random.randint(-n * 2, n + 1)
+        if left - right < n:
+            offset = n - (left - right)
+            expr = "((%s-%s)--%s)" % (left, right, offset)
+        else:
+            offset = (left - right) - n
+            expr = "((%s-%s)-%s)" % (left, right, offset)
+    elif choice == 2:
+        left = random.randint(2, int(n / 2) + 2)
+        right = random.randint(int(n / 5), int(n / 3))
+        if left * right < n:
+            offset = n - (left * right)
+            expr = "((%s*%s)--%s)" % (left, right, offset)
+        else:
+            offset = (left * right) - n
+            expr = "((%s*%s)-%s)" % (left, right, offset)
+
+    return expr
